f53dff8e89f9ebf42171eca84027f0a78d0fb7e9
kosemeci <kosemecialperen37@gmail.com>
2024-07-26 23:37:16 +0300

    files edited

---
+++ public/js/boxes.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const scene = document.getElementById("scene");
+    const answerText = document.getElementById("answerText");
+    const answerSection = document.getElementById("answer-section");
+
+    scene.style.display = "flex";
+    answerText.style.display = "block";
+    answerSection.style.display = "block";
+});
--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,16 @@ const db = require("../data/db");
 const router = express.Router();
 
 
-router.use("/boxes/level_:box_name", (req,res)=>{
-    const boxName = req.params.box_name;
-    res.render("boxes",{ box_name : boxName }); 
+router.use("/boxes/level_:box_name", async (req,res) => {
+
+    try{
+        
+        const [data,]= await db.execute("SELECT * FROM words  where level = ?",[req.params.box_name]);
+        res.render("boxes", {data : data }); 
+    }
+    catch(err){
+        console.log(err);
+    }
 });
 
 router.use("/mybox", (req,res)=>{
